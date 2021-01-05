79984a08cf096fecd8cd70600f529c4875422a72
Michael-Lee-1994 <michael.lee.1994.flatiron@gmail.com>
2021-01-04 16:59:07 -0800

    removing redundant code

--- src/multiplePlacesContainers/TeamContainer.js
+++ src/multiplePlacesContainers/TeamContainer.js
@@ -1,20 +1,3 @@
-// import React, { Component } from 'react';
-// import PetCard from '../components/petComponents/PetCard';
-
-// class TeamContainer extends Component {
-//     render() {
-//         //some function to map pets
-//         return (
-//             <div>
-//                 <h3>Team</h3>
-//                 <PetCard/>
-//             </div>
-//         );
-//     }
-// }
-
-// export default TeamContainer;
-
 import React, { Component } from 'react';
 import PetCard from '../components/petComponents/PetCard';
 
